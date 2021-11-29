bb61ffab8bc2e395d7f3e2248f0a33768fb776e2
Shadee Merhi <shadmerhi@gmail.com>
2021-11-29 00:51:10 -0700

    add namespace to socket connection

--- src/SocketProvider.js
+++ src/SocketProvider.js
@@ -8,7 +8,7 @@ const SocketProvider = ({ id, children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:4000', { query: { id }});
+    const newSocket = io('http://localhost:4000/mediasoup');
     setSocket(newSocket);
 
     return () => {

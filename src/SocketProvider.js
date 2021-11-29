55ae0a7f7c0bd1bbef9bd57c9cf13a1348a5fb11
Shadee Merhi <shadmerhi@gmail.com>
2021-11-28 23:36:17 -0700

    add socket provider

---
+++ src/SocketProvider.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import io from 'socket.io-client';
+
+export const SocketContext = React.createContext();
+
+const SocketProvider = ({ id, children }) => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:4000', { query: { id }});
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+      newSocket.off();
+    }
+  }, []);
+
+  const value = {
+    socket,
+  }
+
+  return (
+    <SocketContext.Provider value={value}>
+      {children}
+    </SocketContext.Provider>
+  )
+
+}
+
+export default SocketProvider;
